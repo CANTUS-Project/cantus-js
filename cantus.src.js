67f64762b3c3c9a9d743ff9e57e2734d490f1bac
Christopher Antila <christopher@antila.ca>
2016-06-02 23:16:38 -0400

    New: Shim or something for Promises
    I'll use the "es6-promise" library to dynamically polyfill the "window" Promise
    object if required.
    
    Closes #1

--- cantus.src.js
+++ cantus.src.js
@@ -22,6 +22,7 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // ------------------------------------------------------------------------------------------------
 
+require('es6-promise').polyfill();
 
 const VALID_FIELDS = [
     'id', 'name', 'description', 'mass_or_office', 'date', 'feast_code', 'incipit', 'source',
